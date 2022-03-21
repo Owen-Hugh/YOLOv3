6c1417d571fc2b1cac68a9b228edef88b7ecb52c
1454674953 <82088452+1454674953@users.noreply.github.com>
2022-03-21 10:43:06 +0800

    Update YoloDemo.py

--- YoloDemo.py
+++ YoloDemo.py
@@ -3,52 +3,52 @@ import time
 import numpy as np
 
 Wide = 320
-confThreshold = 0.5 # 置信度阈值
-nmsThreshold = 0.3 # 非极大值抑制的阈值
+confThreshold = 0.5 # Confidence threshold
+nmsThreshold = 0.3 # Threshold of non maximum suppression
 ftime= 0
 
 cap = cv2.VideoCapture(0)
 
-print("正在加载标签...")
+print("Loading labels...")
 classNames = []
 classFile = 'coco.names'
 with open(classFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
-print("正在加载网络权重...")
+# print("Loading network weights...")
 
-# 在这里加个class选择yolo
+# Add a class here and choose Yolo
 
 modelConfiguration = 'yolov3-tiny.cfg'
 modelWeights = 'yolov3-tiny.weights'
 
-# 创建网络
+# Create network
 net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 # net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)  # gpu
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)  # cpu
-print("加载完毕！")
+# print("Loading completed")
 
 def findObjects(outputs, img):
     """
-    查找对象
-    :param outputs:输出
-    :param img:图像
+    Find object
+    :param outputs:outputs
+    :param img:image
     :return:x,y,w,h
     """
     hT, wT, cT = img.shape
     bbox = [] # x,y,h,w
-    classIds = [] # 类, id
-    confs = [] # 置信度
+    classIds = [] #  id
+    confs = [] # Confidence
 
     for output in outputs:
         for det in output:
             scores = det[5:]
-            classId = np.argmax(scores) # 置信度最大的索引
-            confidence = scores[classId] # 置信度最大的值
+            classId = np.argmax(scores) # Index with the highest confidence
+            confidence = scores[classId] # Maximum confidence value
             if confidence > confThreshold:
                 w,h = int(det[2]*wT) , int(det[3]*hT)
-                x,y = int(det[0]*wT - w/2),int(det[1]*hT - h/2) # 中心点坐标转左上角
+                x,y = int(det[0]*wT - w/2),int(det[1]*hT - h/2) # Center point coordinates turn to the upper left corner
                 bbox.append([x,y,w,h])
                 classIds.append(classId)
                 confs.append(float(confidence))
@@ -72,12 +72,12 @@ while True:
     outputNames = [layerNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
     # print(outputNames)
-    outputs = net.forward(outputNames)  # 边界框、对象名称、ID...
+    outputs = net.forward(outputNames)  # Bounding box, object name, id
     findObjects(outputs,img)
 
     ltime = time.time()
     fps = 1 / (ltime - ftime)
-    ftime = time.time()  # 时间读取
+    ftime = time.time()  # Time reading
     cv2.putText(img, 'FPS: ' + str(int(fps)), (25, 50), cv2.FONT_HERSHEY_PLAIN, 2, (100, 100, 255), 2)
 
     cv2.imshow('img', img)
